de4578ccb36e86abeeffea22e768562a80e44e5e
Labrot <sarah.lientz@gmail.com config --global user.email>
2022-04-27 12:55:06 -0400

    updating resume

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -4,7 +4,7 @@ import CV from '../../assets/Resume.pdf'
 const CTA = () => {
   return (
     <div className='cta'>
-      <a href={CV} download className='btn'>Download Resume</a>
+      <a href={CV} target="_blank" className='btn'>Download Resume</a>
       <a href="#contact" className='btn btn-primary'>Let's Talk</a>
     </div>
   )
